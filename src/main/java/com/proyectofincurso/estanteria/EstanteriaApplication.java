68b79edcd3cc3d55db4090eb686a5f792efa0910
CroquetaConQueso <lacio161@gmail.com>
2026-01-06 17:03:06 +0100

    The first commit

---
+++ src/main/java/com/proyectofincurso/estanteria/EstanteriaApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofincurso.estanteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstanteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstanteriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectofincurso/estanteria/EstanteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofincurso.estanteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstanteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
